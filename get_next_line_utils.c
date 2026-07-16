79075ae260c87914807255030e3d39fb29c523f4
tomanari <tomanari.lol@gmail.com>
2026-07-16 16:51:18 -0300

    gnl utils

---
+++ get_next_line_utils.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtomanar <mtomanar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 16:47:48 by mtomanar          #+#    #+#             */
+/*   Updated: 2026/07/16 16:50:50 by mtomanar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*ptr;
+
+	if (size != 0 && nmemb >= (size_t)-1 / size)
+		return (0);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (0);
+	ft_bzero (ptr, size * nmemb);
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (unsigned char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, s1, ft_strlen(s1) + 1);
+	ft_strlcpy(&(dest[ft_strlen(s1)]), s2, ft_strlen(s2) + 1);
+	return (dest);
+}
