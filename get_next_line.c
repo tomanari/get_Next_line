5b3711e24f1c5fad5270aa8503fd15591edfa8f7
tomanari <tomanari.lol@gmail.com>
2026-07-21 11:11:24 -0300

    extract_line / read_stock

--- get_next_line.c
+++ get_next_line.c
@@ -10,3 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line (int fd)
+{
+	static char	*storage;
+	char				line;
+
+
+}
+
+char	*read_and_stock(int fd, char *static_buffer)
+{
+	char	*buffer;
+	int		count;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = calloc(sizeof(char), (BUFFER_SIZE + 1));	
+	if (!buffer)
+		return(NULL);
+	count + 1;
+	while (!ft_strchr(static_buffer, '\n') && count !=0)
+	{
+		count = read(fd, buffer, BUFFER_SIZE);
+		buffer[count] = '\0';
+		static_buffer = ft_strjoin(static_buffer, buffer);
+	}
+	return (static_buffer);
+}
+
+char	*extract_line(char *static_buffer)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!static_buffer || static_buffer[0] == '\0')
+		return (NULL);
+	while (static_buffer[i] && static_buffer != '\n')
+		i++;
+	line = calloc(sizeof(char), (i + 2));
+	if (!line)
+		return (NULL);
+	while (static_buffer[i] && static_buffer != '\n')
+	{
+		line[i] == static_buffer[i];
+		i++;
+	}
+	if (static_buffer == '\n')
+	{
+		line[i] == static_buffer[i];
+		i++;
+	}
+	line[i] = '/0';
+	return (line);
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -12,9 +12,17 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <string.h>
+
 # include <stdlib.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#	 define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 
 # endif
